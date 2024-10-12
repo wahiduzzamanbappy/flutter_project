e5b3a54cf2510aeb0a54f7bd9271fa1ced9e255a
MD Wahid Bappy <wahidbappy253@gmail.com>
2024-10-12 22:43:20 +0600

    Code added

--- lib/main.dart
+++ lib/main.dart
@@ -28,111 +28,270 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 16,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                showModalBottomSheet(
-                  barrierColor: Colors.green.shade100,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  isScrollControlled: false,
-                  useSafeArea: true,
-                  enableDrag: false,
-                  context: context,
-                  builder: (context) {
-                    return Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                height: 16,
-                              ),
-                              Text(
-                                'Title',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Divider(
-                          height: 10,
-                          thickness: 4,
-                        ),
-                        const Text('Sample'),
-                        Row(
+      body: Scrollbar(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 16,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                    barrierColor: Colors.green.shade100,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    isScrollControlled: false,
+                    useSafeArea: true,
+                    enableDrag: false,
+                    context: context,
+                    builder: (context) {
+                      return Column(
                           children: [
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: const Text('Cancel'),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 16),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                    height: 16,
+                                  ),
+                                  Text(
+                                    'Title',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ],
+                              ),
                             ),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: const Text('Save'),
+                            const Divider(
+                              height: 10,
+                              thickness: 4,
+                            ),
+                            const Text('Sample'),
+                            Row(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Text('Cancel'),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Text('Save'),
+                                ),
+                              ],
                             ),
                           ],
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              child: const Text(
-                'TAP',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            SizedBox(height: 24),
-            Padding(
-              padding: EdgeInsets.all(18),
-              child: TextField(
-                maxLength: 50,
-                onChanged: (String? value) {
-                  print('value');
+                        );
+                    },
+                  );
                 },
-                controller: TextEditingController(),
-                obscureText: false,
-                maxLines: 1,
-                keyboardType: TextInputType.phone,
-                enabled: true,
-                decoration: InputDecoration(
-                  hintText: 'Phone',
-                  hintStyle: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.grey.shade50,
-                  ),
-                  labelText: 'Contact Number',
-                  prefix: Icon(Icons.phone),
-                  suffix: Icon(Icons.person),
-                  fillColor: Colors.white54,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.pink),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 2),
-                  ),
-                  disabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 4),
+                child: const Text(
+                  'TAP',
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 24),
+              Padding(
+                padding: EdgeInsets.all(18),
+                child: TextField(
+                  //maxLength: 50,
+                  onChanged: (String? value) {
+                    print('value');
+                  },
+                  controller: TextEditingController(),
+                  obscureText: false,
+                  maxLines: 1,
+                  keyboardType: TextInputType.emailAddress,
+                  enabled: true,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey.shade50,
+                    ),
+                    labelText: 'EMail Address',
+                    prefix: Icon(Icons.email),
+                    suffix: Icon(Icons.person),
+                    fillColor: Colors.white54,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 2),
+                    ),
+                    /*disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 4),
+                    ),*/
+                  ),
+                ),
+
+              ),
+              Padding(
+                padding: EdgeInsets.all(18),
+                child: TextField(
+                  //maxLength: 50,
+                  onChanged: (String? value) {
+                    print('value');
+                  },
+                  controller: TextEditingController(),
+                  obscureText: true,
+                  maxLines: 1,
+                  keyboardType: TextInputType.visiblePassword,
+                  enabled: true,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey.shade50,
+                    ),
+                    labelText: 'Password',
+                    prefix: Icon(Icons.phone),
+                    suffix: Icon(Icons.person),
+                    fillColor: Colors.white54,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 4),
+                    ),
+                    /*disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 4),
+                    ),*/
+                  ),
+                ),
+
+              ),
+              Padding(
+                padding: EdgeInsets.all(18),
+                child: TextField(
+                  //maxLength: 50,
+                  onChanged: (String? value) {
+                    print('value');
+                  },
+                  controller: TextEditingController(),
+                  obscureText: false,
+                  maxLines: 1,
+                  keyboardType: TextInputType.text,
+                  enabled: true,
+                  decoration: InputDecoration(
+                    hintText: 'Full Name',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey.shade50,
+                    ),
+                    labelText: 'Full Name',
+                    prefix: Icon(Icons.phone),
+                    suffix: Icon(Icons.person),
+                    fillColor: Colors.white54,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 4),
+                    ),
+                    /*disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 4),
+                    ),*/
+                  ),
+                ),
+
+              ),
+              Padding(
+                padding: EdgeInsets.all(18),
+                child: TextField(
+                  //maxLength: 50,
+                  onChanged: (String? value) {
+                    print('value');
+                  },
+                  controller: TextEditingController(),
+                  obscureText: false,
+                  maxLines: 1,
+                  keyboardType: TextInputType.text,
+                  enabled: true,
+                  decoration: InputDecoration(
+                    hintText: 'Last Name',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey.shade50,
+                    ),
+                    labelText: 'Last Name',
+                    prefix: Icon(Icons.phone),
+                    suffix: Icon(Icons.person),
+                    fillColor: Colors.white54,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 4),
+                    ),
+                    /*disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 4),
+                    ),*/
+                  ),
+                ),
+
+              ),
+              Padding(
+                padding: EdgeInsets.all(18),
+                child: TextField(
+                  //maxLength: 50,
+                  onChanged: (String? value) {
+                    print('value');
+                  },
+                  controller: TextEditingController(),
+                  obscureText: false,
+                  maxLines: 1,
+                  keyboardType: TextInputType.phone,
+                  enabled: true,
+                  decoration: InputDecoration(
+                    hintText: 'Contact Number',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey.shade50,
+                    ),
+                    labelText: 'Contact Number',
+                    prefix: Icon(Icons.phone),
+                    suffix: Icon(Icons.person),
+                    fillColor: Colors.white54,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 4),
+                    ),
+                    /*disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 4),
+                    ),*/
+                  ),
+                ),
+
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(onPressed: () {
