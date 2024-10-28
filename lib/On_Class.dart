f3cce3506f8ad7ba1e41c9cde406ebeabb8e5d9a
MD Wahid Bappy <wahidbappy253@gmail.com>
2024-10-28 20:41:40 +0600

    Code added

--- lib/On_Class.dart
+++ lib/On_Class.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// Divider, ModalBottomSheet, TextField
+// Gridview, ListTile, Form, GlobalKey, Key
 
 void main() {
   runApp(HelloWorldApp());
@@ -18,7 +18,26 @@ class HelloWorldApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  List<String> friendList = [
+    'Iram',
+    'Shabbin',
+    'Rakib',
+    'Hasan',
+    'Roy',
+    'Niloy',
+    'Niloy',
+    'Niloy',
+    'Niloy',
+    'Niloy',
+    'Niloy',
+    'Niloy',
+    'Niloy',
+  ];
+  TextEditingController _emailTEController = TextEditingController();
+  TextEditingController _passwordTEController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -27,119 +46,111 @@ class Home extends StatelessWidget {
         title: Text('Home'),
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 100),
-            ElevatedButton(
-              onPressed: () {
-                // showAboutDialog(context: context);
-                showModalBottomSheet(
-                  // barrierColor: Colors.grey.shade50,
-                  // backgroundColor: Colors.green.shade200,
-                  context: context,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  isScrollControlled: true,
-                  useSafeArea: true,
-                  enableDrag: false,
-                  builder: (ctx) {
-                    return Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Title',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Divider(
-                          height: 20,
-                          thickness: 4,
-                        ),
-                        Text('Sample'),
-                        Row(
-                          children: [
-                            ElevatedButton(
-                                onPressed: () {}, child: Text('Cancel')),
-                            ElevatedButton(
-                                onPressed: () {}, child: Text('Save')),
-                          ],
-                        )
-                      ],
-                    );
-                  },
-                );
-              },
-              child: Text('Show dialog'),
-            ),
-            SizedBox(height: 24),
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: TextField(
-                maxLength: 50,
-                onChanged: (String? value) {
-                  print(value);
+      // body: ListView.builder(
+      //   itemCount: friendList.length,
+      //   itemBuilder: (context, index) {
+      //     return ListTile(
+      //       title: Text(friendList[index]),
+      //       subtitle: Text('Friend no $index'),
+      //       trailing: Icon(Icons.arrow_forward),
+      //       leading: CircleAvatar(
+      //         child: Icon(Icons.person),
+      //       ),
+      //       onTap: () {
+      //         print('on tap $index');
+      //       },
+      //       onLongPress: () {
+      //         print('on long press $index');
+      //       },
+      //       // tileColor: Colors.white10,
+      //       contentPadding: EdgeInsets.symmetric(
+      //         horizontal: 24,
+      //         vertical: 4
+      //       ),
+      //       // enabled: false,
+      //       dense: false, // TODO: need to explain
+      //       titleTextStyle: TextStyle(
+      //         fontSize: 24,
+      //         color: Colors.pink
+      //       ),
+      //       subtitleTextStyle: TextStyle(
+      //         fontSize: 12,
+      //         color: Colors.blue
+      //       ),
+      //       // selected: true,
+      //       // selectedColor: Colors.pink,
+      //     );
+      //   },
+      // ),
+      // body: GridView(
+      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //     crossAxisCount: 2,
+      //     mainAxisSpacing: 12,
+      //     crossAxisSpacing: 8
+      //   ),
+      //   children: [
+      //     Text('dsfdf'),
+      //     Text('dsfdf'),
+      //     Text('dsfdf'),
+      //     Text('dsfdf'),
+      //   ],
+      // ),
+      // body: GridView.builder(
+      //     gridDelegate:
+      //         SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+      //     itemCount: friendList.length,
+      //     itemBuilder: (context, index) {
+      //       return Center(child: Text(friendList[index]));
+      //     },
+      // ),
+      body: Padding(
+        key: Key('my-padding-key'),
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _emailTEController,
+                decoration: InputDecoration(hintText: 'Email'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter your email';
+                  }
+                  return null;
                 },
-                controller: TextEditingController(),
-                // obscureText: true,
-                maxLines: 1,
-                keyboardType: TextInputType.phone,
-                enabled: true,
-                decoration: InputDecoration(
-                  hintText: 'Phone',
-                  hintStyle: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    color: Colors.grey.shade400,
-                  ),
-                  labelText: 'Phone number',
-                  // label: Icon(Icons.add),
-                  prefixIcon: Icon(Icons.phone),
-                  suffixIcon: Icon(Icons.person),
-                  fillColor: Colors.white54,
-                  filled: true,
-                  // counterStyle: TextStyle(
-                  //   fontSize: 24
-                  // ),
-                  // counterText: '',
-                  // prefix: Column(
-                  //   children: [
-                  //     Text('d'),
-                  //     Text('b'),
-                  //   ],
-                  // ),
-                  // suffix: Column(
-                  //   children: [
-                  //     Text('d'),
-                  //     Text('b'),
-                  //   ],
-                  // ),
-                  // icon: IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red)),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green, width: 2),
-                  ),
-                  disabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey, width: 4),
-                  ),
-                ),
               ),
-            ),
-          ],
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                controller: _passwordTEController,
+                decoration: InputDecoration(hintText: 'Password'),
+                validator: (String? value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Enter your password';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // String email = _emailTEController.text;
+                  // String password = _passwordTEController.text;
+                  // if (email.isNotEmpty && password.isNotEmpty) {
+                  //   print('Login success');
+                  // } else {
+                  //   print('Login failed. Missing data');
+                  // }
+                  if (_formKey.currentState!.validate()) {
+                    print('Login success');
+                  }
+                },
+                child: Text('Tap'),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-// Suggestion => ctl + space
--- lib/main.dart
+++ lib/main.dart
@@ -1,308 +1,145 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DemoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DemoApp extends StatelessWidget {
+  DemoApp({super.key});
+
+  List<String> friendList = [
+    'Iram',
+    'Sabbir',
+    'Rakib',
+    'Hasan',
+    'Roy',
+    'Niloy',
+    'Kopil',
+    'Umama',
+    'Bappy',
+    'Antor',
+    'Saiful',
+    'Saiful',
+    'Saiful',
+    'Saiful',
+    'Saiful',
+  ];
+
+  TextEditingController _emailTEController = TextEditingController();
+  TextEditingController _passswordTEController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
-      body: Scrollbar(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+      title: 'Demo App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Home',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+          elevation: 10,
+        ),
+        /*  body: ListView.builder(
+          itemCount: friendList.length,
+          itemBuilder: (context, index) {
+            int displayIndex = index + 1;
+            return ListTile(
+              dense: false,
+              title: Text(friendList[index]),
+              subtitle: Text('Friend no $displayIndex'),
+              trailing: const Icon(Icons.arrow_circle_right_outlined),
+              leading: const CircleAvatar(
+                backgroundColor: Colors.amber,
+                child: Icon(Icons.person),
+              ),
+              onTap: () {
+                print('On Tap $index');
+              },
+              onLongPress: () {
+                print('On LongPress $index');
+              },
+              tileColor: Colors.grey,
+            );
+          },
+        ),*/ /*
+        body: Center(
+          child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 12, crossAxisSpacing: 8),
             children: [
-              const SizedBox(
-                height: 16,
+              Text('Iram'),
+              Text('Sabbir'),
+              Text('Rakib'),
+              Text('Hasan'),
+              Text('Hasan'),
+              Text('Hasan'),
+              Text('Hasan'),
+              Text('Hasan'),
+              Text('Hasan'),
+            ],*/
+        /*body: GridView.builder(
+          gridDelegate:
+              const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          itemCount: friendList.length,
+          itemBuilder: (context, index) {
+            return Center(
+              child: Text(friendList[index]),
+            );
+          },
+        ),*/
+        body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: _emailTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter Email Address';
+                    }
+                    return null;
+                  }),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator:(String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter Password';
+                  }
+                  return null;
+                } ,
+                controller: _passswordTEController,
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
               ),
               ElevatedButton(
                 onPressed: () {
-                  showModalBottomSheet(
-                    barrierColor: Colors.green.shade100,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    isScrollControlled: false,
-                    useSafeArea: true,
-                    enableDrag: false,
-                    context: context,
-                    builder: (context) {
-                      return Column(
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.symmetric(vertical: 16),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    height: 16,
-                                  ),
-                                  Text(
-                                    'Title',
-                                    style: TextStyle(fontSize: 18),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            const Divider(
-                              height: 10,
-                              thickness: 4,
-                            ),
-                            const Text('Sample'),
-                            Row(
-                              children: [
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  child: const Text('Cancel'),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  child: const Text('Save'),
-                                ),
-                              ],
-                            ),
-                          ],
-                        );
-                    },
-                  );
+                  /*String email = _emailTEController.text;
+                  String password = _passswordTEController.text;
+
+                  if (email.isNotEmpty && password.isNotEmpty) {
+                    print('Login Success');
+                  } else {
+                    print('Login Failed. Missing Data');
+                  }*/
+                  if (_formKey.currentState!.validate()) {
+                    print('Login Success');
+                  }
                 },
-                child: const Text(
-                  'TAP',
-                  style: TextStyle(
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24),
-              Padding(
-                padding: EdgeInsets.all(18),
-                child: TextField(
-                  //maxLength: 50,
-                  onChanged: (String? value) {
-                    print('value');
-                  },
-                  controller: TextEditingController(),
-                  obscureText: false,
-                  maxLines: 1,
-                  keyboardType: TextInputType.emailAddress,
-                  enabled: true,
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    hintStyle: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey.shade50,
-                    ),
-                    labelText: 'EMail Address',
-                    prefix: Icon(Icons.email),
-                    suffix: Icon(Icons.person),
-                    fillColor: Colors.white54,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 2),
-                    ),
-                    /*disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 4),
-                    ),*/
-                  ),
-                ),
-
-              ),
-              Padding(
-                padding: EdgeInsets.all(18),
-                child: TextField(
-                  //maxLength: 50,
-                  onChanged: (String? value) {
-                    print('value');
-                  },
-                  controller: TextEditingController(),
-                  obscureText: true,
-                  maxLines: 1,
-                  keyboardType: TextInputType.visiblePassword,
-                  enabled: true,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    hintStyle: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey.shade50,
-                    ),
-                    labelText: 'Password',
-                    prefix: Icon(Icons.phone),
-                    suffix: Icon(Icons.person),
-                    fillColor: Colors.white54,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 4),
-                    ),
-                    /*disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 4),
-                    ),*/
-                  ),
-                ),
-
-              ),
-              Padding(
-                padding: EdgeInsets.all(18),
-                child: TextField(
-                  //maxLength: 50,
-                  onChanged: (String? value) {
-                    print('value');
-                  },
-                  controller: TextEditingController(),
-                  obscureText: false,
-                  maxLines: 1,
-                  keyboardType: TextInputType.text,
-                  enabled: true,
-                  decoration: InputDecoration(
-                    hintText: 'Full Name',
-                    hintStyle: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey.shade50,
-                    ),
-                    labelText: 'Full Name',
-                    prefix: Icon(Icons.phone),
-                    suffix: Icon(Icons.person),
-                    fillColor: Colors.white54,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 4),
-                    ),
-                    /*disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 4),
-                    ),*/
-                  ),
-                ),
-
-              ),
-              Padding(
-                padding: EdgeInsets.all(18),
-                child: TextField(
-                  //maxLength: 50,
-                  onChanged: (String? value) {
-                    print('value');
-                  },
-                  controller: TextEditingController(),
-                  obscureText: false,
-                  maxLines: 1,
-                  keyboardType: TextInputType.text,
-                  enabled: true,
-                  decoration: InputDecoration(
-                    hintText: 'Last Name',
-                    hintStyle: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey.shade50,
-                    ),
-                    labelText: 'Last Name',
-                    prefix: Icon(Icons.phone),
-                    suffix: Icon(Icons.person),
-                    fillColor: Colors.white54,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 4),
-                    ),
-                    /*disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 4),
-                    ),*/
-                  ),
-                ),
-
-              ),
-              Padding(
-                padding: EdgeInsets.all(18),
-                child: TextField(
-                  //maxLength: 50,
-                  onChanged: (String? value) {
-                    print('value');
-                  },
-                  controller: TextEditingController(),
-                  obscureText: false,
-                  maxLines: 1,
-                  keyboardType: TextInputType.phone,
-                  enabled: true,
-                  decoration: InputDecoration(
-                    hintText: 'Contact Number',
-                    hintStyle: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey.shade50,
-                    ),
-                    labelText: 'Contact Number',
-                    prefix: Icon(Icons.phone),
-                    suffix: Icon(Icons.person),
-                    fillColor: Colors.white54,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 4),
-                    ),
-                    /*disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 4),
-                    ),*/
-                  ),
-                ),
-
-              ),
+                child: const Text('Tap'),
+              )
             ],
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-
-      },
-        backgroundColor: Colors.amberAccent,
-        foregroundColor: Colors.black,
-        //hoverColor: Colors.blue,
-        splashColor: Colors.green,
-      child:
-      const Icon(Icons.add),
       ),
     );
   }
